ab05c7e34af6409751e9d2e20ae75ded92efde7c
seantindell@gmail.com <seantindell@gmail.com>
2017-05-08 19:53:32 +0000

    minor tweaks and upadting work page

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,9 @@ $(document).ready(function(){
         $('.line2').animate ({ top: '50px'}, 600, 'swing', function() {
             loop();
         });
+
     }
+
      $('.active').append('<span class ="line"></span><span class ="line2"></span>');
      loop();
      
@@ -33,25 +35,23 @@ $(document).ready(function(){
          $('.hoverLine').remove();
         
      });
+     circle();
 
 });
 
 
-var t = 0;
-
 function circle()  {
-    t += 0.01;
 
-    var radius = 175;
-        var fields = $('.landingItem'), 
-            container = $('#landingContainer'), 
+    var radius = 200;
+        var fields = $('.workItem'), 
+            container = $('#workContainer'), 
             width = container.width(), 
             height = container.height();
         var angle = 0, 
             step = (2*Math.PI) / fields.length;
         fields.each(function() {
-            var x = Math.round(width/2 + radius * Math.cos(angle) - $(this).width()/2)+50;
-            var y = Math.round(height/2 + radius * Math.sin(angle) - $(this).height()/2)+75;
+            var x = Math.round(width/2 + radius * Math.cos(angle) - $(this).width()/2)+0;
+            var y = Math.round(height/2 + radius * Math.sin(angle) - $(this).height()/2)+350;
             
             $(this).css({
                 left: x + 'px',
@@ -62,38 +62,8 @@ function circle()  {
 
 }
 
-    
-$(document).ready(function() {
-    circle();
-   
-});
 
 
 
 
 
-
-
-// var t = 0;
-
-// function moveit(id, xC, yC  {
-//     t += 0.01;
-
-//     var r = 150;
-//     var xcenter = xC;
-//     var ycenter = yC;
-//     var newLeft = Math.floor(xcenter + (r * Math.cos(t)));
-//     var newTop = Math.floor(ycenter + (r * Math.sin(t)));
-//     $(id).animate({
-//         top: newTop,
-//         left: newLeft,
-//     }, 1, function() {
-//         moveit(id, xC, yC);
-//     });
-// }
-
-// $(document).ready(function() {
-//     moveit('#landingWork', 150, 300); 
-//     moveit('#landingAbout', 150,300); 
-//     moveit('#landingContact', 150,300);
-// });
