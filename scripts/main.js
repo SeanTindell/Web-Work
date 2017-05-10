72568b4644f186db2771edbf33bc0e809cae0721
seantindell@gmail.com <seantindell@gmail.com>
2017-05-10 14:05:15 +0000

    removed some java and edited the work page. Cleaned up the css and Js file.

--- scripts/main.js
+++ scripts/main.js
@@ -15,9 +15,7 @@ $(document).ready(function(){
         $('.line2').animate ({ top: '50px'}, 600, 'swing', function() {
             loop();
         });
-
     }
-
      $('.active').append('<span class ="line"></span><span class ="line2"></span>');
      loop();
      
@@ -35,32 +33,9 @@ $(document).ready(function(){
          $('.hoverLine').remove();
         
      });
-     circle();
-
 });
 
 
-function circle()  {
-
-    var radius = 200;
-        var fields = $('.workItem'), 
-            container = $('#workContainer'), 
-            width = container.width(), 
-            height = container.height();
-        var angle = 0, 
-            step = (2*Math.PI) / fields.length;
-        fields.each(function() {
-            var x = Math.round(width/2 + radius * Math.cos(angle) - $(this).width()/2)+0;
-            var y = Math.round(height/2 + radius * Math.sin(angle) - $(this).height()/2)+350;
-            
-            $(this).css({
-                left: x + 'px',
-                top: y + 'px'
-            });
-            angle += step;
-        });
-
-}
 
 
 
